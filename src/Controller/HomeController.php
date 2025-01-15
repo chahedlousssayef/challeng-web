88f0cbbb85460f6bf4d9a6e626af095c44809fa0
arcabob <159237376+arca-bob@users.noreply.github.com>
2025-01-15 11:50:30 +0100

    Index and contact pages
    Add the home page and the contact page with the project structure

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// ...
+
+class HomeController extends AbstractController
+{
+    #[Route('/template/home/index.html.twig')]
+    public function index(): Response
+    {
+        $text = "Page d'acceuil";
+
+        return $this->render('home/index.html.twig', [
+            'text' => $text,
+        ]);
+    }
+}
---
+++ tests/Controller/ContactControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class ContactControllerTest extends WebTestCase{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/contact');
+
+        self::assertResponseIsSuccessful();
+    }
+}
