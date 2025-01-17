e97451b5994cc8f31aa38ffbdda16c3328d3f5e9
arcabob <159237376+arca-bob@users.noreply.github.com>
2025-01-17 09:09:27 +0100

    add user provider
    add the user provider for the login

---
+++ src/Security/UserProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserProvider implements UserProviderInterface
+{
+    public function loadUserByUsername(string $username): UserInterface
+    {
+        // Chargez l'utilisateur à partir de la base de données ou d'une autre source
+        // Par exemple, utilisez un repository pour trouver l'utilisateur par email
+        return new User();
+    }
+    public function loadUserByIdentifier(string $identifier): UserInterface
+{
+    // Chargez l'utilisateur à partir de la base de données ou d'une autre source
+    // Par exemple, utilisez un repository pour trouver l'utilisateur par email
+    return new User();
+}
+
+
+    public function refreshUser(UserInterface $user): UserInterface
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass(string $class): bool
+    {
+        return User::class === $class;
+    }
+}
