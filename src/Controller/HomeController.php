b48a5b1ffba5a3c9c7bf87909cdc552102aae697
Badis91 <146003225+Badis91@users.noreply.github.com>
2025-01-19 14:58:14 +0100

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,1 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return new Response('<h1>Bienvenue sur la page d\'accueil</h1>');
+    }
+}
 
