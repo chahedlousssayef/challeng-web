b330ec206023073de9d039698024d5e69027ef26
Adam1832 <159237282+Adam1832@users.noreply.github.com>
2025-01-19 21:17:54 +0100

    Update ContactControllerTest.php

--- tests/Controller/ContactControllerTest.php
+++ tests/Controller/ContactControllerTest.php
@@ -4,12 +4,53 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-final class ContactControllerTest extends WebTestCase{
-    public function testIndex(): void
-    {
-        $client = static::createClient();
-        $client->request('GET', '/contact');
+class ContactControllerTest extends WebTestCase
+{
 
-        self::assertResponseIsSuccessful();
+    // Teste le rendu du template pour la page de contact
+    public function testContactPageRendering(): void
+    {
+        // Crée un client HTTP
+        $client = static::createClient();
+    
+        // Charge la page contact qui utilise base.html.twig
+        $crawler = $client->request('GET', '/contact');
+    
+        // Vérifie que la réponse HTTP est réussie (200 OK)
+        $this->assertResponseIsSuccessful();
+    
+        // Vérifie les éléments du template
+        $this->customAssertPageTitleContains($crawler,'Contact');
+        $this->assertHeaderExists($crawler);
+        $this->assertFooterContains($crawler, 'A propos');
+        $this->assertMainContentContains($crawler, 'Comment nous contactez ?');
+    }
+    
+    // Vérifie que le titre de la page contient le texte attendu.
+    public function customAssertPageTitleContains($crawler, string $expectedTitle): void
+    {
+        $this->assertStringContainsString($expectedTitle, $crawler->filter('title')->text());
+    }
+
+    // Vérifie que l'en-tête de la page contient les éléments attendus.
+    private function assertHeaderExists($crawler): void
+    {
+        $this->assertGreaterThan(0, $crawler->filter('header')->count(), 'Header should be present.');
+        $this->assertSelectorTextContains('header', 'Connexion');
+        $this->assertSelectorTextContains('header', 'Mon compte');
+        $this->assertSelectorTextContains('header', 'Nous Contactez');
+    }
+    
+    // Vérifie que le footer est présent
+    private function assertFooterContains($crawler, string $expectedText): void
+    {
+        $this->assertGreaterThan(0, $crawler->filter('footer')->count(), 'Footer should be present.');
+        $this->assertSelectorTextContains('footer', $expectedText);
+    }
+    
+    // Vérifie que le contenu principal contient le texte attendu.
+    private function assertMainContentContains($crawler, string $expectedText): void
+    {
+        $this->assertSelectorTextContains('main', $expectedText);
     }
 }
